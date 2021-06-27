03a1ce908e33dc0ce9abd7e05e429547b93104b9
venkatesh kuppusamy <kuppusamy_v@subway.com>
2021-06-27 14:09:59 +0530

    Basket Repository changes

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,40 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache redisCache;
+
+        public BasketRepository(IDistributedCache redisCache)
+        {
+            this.redisCache = redisCache ?? throw new ArgumentNullException(nameof(redisCache));
+        }
+
+        public async Task DeleteBasket(string userName)
+        {
+            await redisCache.RemoveAsync(userName);
+        }
+
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await redisCache.GetStringAsync(userName);
+            if (string.IsNullOrWhiteSpace(basket)){
+                return null;
+            }
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart shoppingCart)
+        {
+            await redisCache.SetStringAsync(shoppingCart.Username,JsonConvert.SerializeObject(shoppingCart));
+            return await GetBasket(shoppingCart.Username);
+        }
+    }
+}
