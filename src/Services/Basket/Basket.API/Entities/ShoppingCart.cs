f0fcbab865f8d401a027e27f21a009b8f8f635aa
venkatesh kuppusamy <kuppusamy_v@subway.com>
2021-06-27 13:23:44 +0530

    entities folder.

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string Username { get; set; }
+        public List<ShoppingCartItem> items { get; set; } = new List<ShoppingCartItem>();
+        public ShoppingCart()
+        {
+        }
+
+        public ShoppingCart(string username)
+        {
+            Username = username;
+        }
+
+        public decimal TotalPrice { 
+            get {
+                decimal totalPrice = 0;
+                foreach (var item in items)
+                {
+                    totalPrice = totalPrice + item.Price;
+                }
+                return totalPrice;
+            } 
+        }
+
+
+    }
+}
