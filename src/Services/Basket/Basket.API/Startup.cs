31e1e96a77ee80983d5771bd91c3e77c35a108b3
venkatesh kuppusamy <kuppusamy_v@subway.com>
2021-06-27 13:38:22 +0530

    Redis cache configuration.

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -25,7 +25,7 @@ namespace Basket.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddStackExchangeRedisCache(o => { o.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionString"); });
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
